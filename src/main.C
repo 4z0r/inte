dd02ad5f285771df4cfd7f1d26b58dba484166f2
Ovidiu - Dan Bogat <bogat_ovidiu@hotmail.com>
2013-01-10 17:38:00 +0100

    created main loop

--- src/main.C
+++ src/main.C
@@ -6,6 +6,37 @@
 
 int main(int argc, char *argv[], char *envp[])
 {
-  printf("all set up.\n");
+  char *input = (char*) malloc(256);
+
+  printf("%s ", SHELL_CHARACTER);
+
+  while(fscanf(stdin, "%s", input) == 1)
+    {
+      //quit 
+      if(!strcmp(input, "quit")
+	 || !strcmp(input, "exit")
+	 || !strcmp(input, "q"))
+	{
+	  break;
+	}
+
+      //help
+      else if(!strcmp(input, "help"))
+	{
+	  printf("Print the help.\n");
+	}
+
+      //unknown
+      else
+	{
+	  printf("[INFO] Command %s unknown.\n", input);
+	}
+      printf("%s ", SHELL_CHARACTER);
+    }
+  
+  if(input) free(input);
+  
+  printf("Good Bye!\n");
+
   return(EXIT_SUCCESS);
 }
