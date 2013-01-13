2ee8d045d3d6eda4032a78d926514ef5ebcb664a
Ovidiu - Dan Bogat <bogat_ovidiu@hotmail.com>
2013-01-13 20:12:31 +0100

    added valgrind output to the log

--- scripts/log_mem.sh
+++ scripts/log_mem.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 valgrind --leak-check=full -v ./inte &> log/valgrind/valgrind_`uname -s`_`uname -p`_`uname -r`.log
 echo "Valgrinded on: " `date` >> log/valgrind/valgrind_`uname -s`_`uname -p`_`uname -r`.log
