d501720166ee889d0af0ed251aba847de4a7a373
Ovidiu - Dan Bogat <ovidiu@members.fsf.org>
2013-01-14 21:18:45 +0100

    one last commit

--- src/sockets.C
+++ src/sockets.C
@@ -11,10 +11,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define BUFF 2048
+
 void start_socket_example()
 {
   struct addrinfo hints, *res;
-  int socket1, r2;
+  int socketfd, r2;
   char ipstr[INET6_ADDRSTRLEN];
 
   memset(&hints, 0, sizeof(hints));
@@ -29,24 +31,24 @@ void start_socket_example()
       return;
     }
 
-  if ((socket1 = socket(res->ai_family, res->ai_socktype,
+  if ((socketfd = socket(res->ai_family, res->ai_socktype,
 		       res->ai_protocol)) == -1) {
     printf("client: socket\n");
   }
   void *addr = NULL;
-  if (res->ai_family == AF_INET) { // IPv4
-    struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
-    addr = &(ipv4->sin_addr);
-  
-  } else { // IPv6
+  if (res->ai_family == AF_INET) 
+    { 
+      struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
+      addr = &(ipv4->sin_addr);
+    }else{ 
     struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)res->ai_addr;
     addr = &(ipv6->sin6_addr);
-  
   }
+
   inet_ntop(res->ai_family, addr, ipstr, sizeof(ipstr));
   printf("Connecting to: %s ...\n", ipstr);
 
-  if(connect(socket1, res->ai_addr, res->ai_addrlen) == -1)
+  if(connect(socketfd, res->ai_addr, res->ai_addrlen) == -1)
     {
       printf("Error %s\n", clean_errno()); //connect sets errno
       freeaddrinfo(res);
@@ -57,20 +59,20 @@ void start_socket_example()
 
   printf("Sending GET /index.html HTTP/1.0\n");
   
-  char *buffer = (char *) malloc(2048);
+  char *buffer = (char *) malloc(BUFF);
   strcpy(buffer, "GET /index.html HTTP/1.0\n\r\n\r");
-  send(socket1, buffer, strlen(buffer), 0);
+  send(socketfd, buffer, strlen(buffer), 0);
   
-  int r;
-  r=recv(socket1, buffer, 2048-1, 0);
+  int rcvfd;
+  rcvfd=recv(socketfd, buffer, BUFF-1, 0);
 
-  if(r > 0)
-    buffer[r] = '\0';
+  if(rcvfd > 0)
+    buffer[rcvfd] = '\0';
 
   printf("GOT: %s\n", buffer);
 
   free(buffer);
-  close(socket1);
+  close(socketfd);
 
   freeaddrinfo(res);
 }
